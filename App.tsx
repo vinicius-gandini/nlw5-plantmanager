6a2baaf829617c4152ebdce1e1af9f50fa4df55d
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-19 23:32:49 -0300

    primeira aula finalizada :ok_hand:

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
-    <View>
-      <Text>
-        Hello World
-      </Text>
-    </View>
+    <Welcome />
   )
 }
