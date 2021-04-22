239a5307021546c375d17a3474fc996c676f6d75
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-21 22:24:06 -0300

    dia 03 finalizado :ok_hand:

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá</Text>
+        <Text style={styles.userName}>Vinicius</Text>
+      </View>
+
+      <Image source={{ uri: "jfdgd" }} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  }
+})
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
@@ -6,6 +7,12 @@ import fonts from '../../styles/fonts';
 import { Button } from '../components/Button';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -18,7 +25,7 @@ export function Confirmation() {
         <Text style={styles.subtitle}>Agora vamos começar a cuidar das suas plantinhas com muito cuidado</Text>
 
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
