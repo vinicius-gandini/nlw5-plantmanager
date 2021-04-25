1c8dc147fc12f9026e05379164a9a0b329ea4a19
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-24 21:08:03 -0300

    dia 05 completo, aplicacao finalizada :ok_hand:

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './src/routes';
+import * as Notifications from 'expo-notifications';
+
 import AppLoading from 'expo-app-loading';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+      }
+    )
+    return () => subscription.remove();
+  }, [])
 
   if(!fontsLoaded)
     return (
--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, View } from 'react-native';
 import colors from '../../styles/colors';
 import { Header } from '../components/Header';
 
 import waterdrop from '../../assets/waterdrop.png';
 import { FlatList } from 'react-native-gesture-handler';
-import { loadPlant, PlantProps } from '../libs/storage';
+import { loadPlant, PlantProps, removePlant, StoragePlantProps } from '../libs/storage';
 import { formatDistance } from 'date-fns/esm';
 import { pt } from 'date-fns/locale';
 import fonts from '../../styles/fonts';
 import { PlantCardSecondary } from '../components/PlantCardSecondary';
+import { Load } from '../components/Load';
 
 export function MyPlants() {
   const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [nextWaterd, setNextWaterd] = useState<string>();
+
+  function handleRemove(plant: PlantProps) {
+    Alert.alert('Remover', `Deseja remover a ${plant.name}?`, [
+      {
+        text: 'Não 🙏🏻',
+        style: 'cancel'
+      },
+      {
+        text: 'Sim 😥',
+        onPress: async () => {
+          try {
+            await removePlant(plant.id);
+
+            setMyPlants((oldData) => 
+              oldData.filter(item => item.id !== plant.id)
+            );
+
+          } catch(error) {
+            Alert.alert('Não foi possível remover!')
+          } 
+        }
+      }
+    ])
+  }
 
   useEffect(() => {
     async function loadStorageData() {
@@ -36,6 +61,8 @@ export function MyPlants() {
     loadStorageData();
   }, [])
 
+  if(loading)
+    return <Load />
   return (
     <View style={styles.container}>
       <Header />
@@ -54,7 +81,7 @@ export function MyPlants() {
           data={myPlants}
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-            <PlantCardSecondary data={item} />
+            <PlantCardSecondary data={item} handleRemove={() => handleRemove(item)}/>
           )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ flex: 1 }}
